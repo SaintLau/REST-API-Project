aa92e6fcfc5d9035748109f70fe1984dc1142a67
laura.santos <laura-santos@future-healthcare.eu>
2022-05-16 16:07:27 +0100

    first commit - project already setup with REST

---
+++ src/test/java/APITests.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class APITests {
+
+    @Test
+
+    void test1() {
+        /*api test - go to google and search for a 'sample rest api for testing'
+        https://reqres.in/
+        Scroll down to 'Give it a Try' -> GET List Users
+        */
+
+        
+    }
+}
